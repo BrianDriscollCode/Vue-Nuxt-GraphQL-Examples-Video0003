b756158fb6e40740bdc2bfafbbed73bc2523c713
briandriscollcode <bdriscoll407@gmail.com>
2023-03-09 10:28:45 -0700

    First commit

---
+++ app.vue
@@ -0,0 +1,20 @@
+<template>
+    <p v-for="(item, name) in data.characters.results"> Name: {{ item.name }} | Gender: {{ item.gender }} </p>
+</template>
+
+<script setup>
+  const getCharacters = gql`
+	query {
+		characters {
+			results {
+				name,
+				gender
+			}
+		}
+	}
+  `
+  const variables = { limit: 40 }
+  const { data } = await useAsyncQuery(getCharacters, variables)
+  console.log(data.value);
+
+  </script>
